c6b9e8ecf9e2a08b7766cb17d3020f223bae7cc6
Bob Codes <bobcodes@outlook.com>
2013-06-14 23:54:22 -0700

    protocol changed - server dropped me
    wrote 17 HelloAction [buildVersion=14.1, whereToSendPlayer=-2,
    keyTime=-1, key=[], somethingThatsEmpty1=, rotmgUrlLibParamEntryPoint=,
    gameNet=rotmg, somethingThatsEmpty2=, playPlatform=rotmg,
    somethingThatsEmpty3=]
    going to use wireshark to record the conversation between the server and
    client
    
    got incoming MapInfoAction 60 53 MapInfoAction [width=256, height=256,
    name=Nexus, momebujot={nexus.Nexus}, dem=-1, seedForRandom=425980924,
    background=2, allowPlayerToTeleport=true, showDisplays=true,
    clientXML=[], extraXML=[]]
    wrote 45 LoadAction [charId=1]

--- protocol-java-impl/src/rotmg/net/RotmgParameters.java
+++ protocol-java-impl/src/rotmg/net/RotmgParameters.java
@@ -40,7 +40,7 @@ public class RotmgParameters {
     public static final Cipher INCOMING_KEY;
     public static final String ROTMG_PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\n"+"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDEmgEmQcgLd0mvWqL6AKmhzj"+"JfZoAmZC0PUmG8K9CB1Ml68P00S3eU+TSL5aG8Mg3Tipvs02gC2veC10knRi7r"+"EsUwL8+h22EsjnpKZ/7K5YV9cefryTMnS0x4QGZbSkdPz/rLh0uGwk8Zu0cEKb"+"xQyvd3+pSmqZ5/ZQGaFjm9TQIDAQAB\n"+"-----END PUBLIC KEY-----";
     
-    public static final String BUILD_VERSION = "13.4";
+    public static final String BUILD_VERSION = "14.1";
     public static int PORT = 2050;
     
     static {
